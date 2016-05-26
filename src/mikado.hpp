46b01edcc46d373fd6c4d744b0b07dac22e18b8e
Nico Schlömer <nico.schloemer@gmail.com>
2016-05-26 22:56:27 +0200

    do something in the dummy test

--- src/mikado.hpp
+++ src/mikado.hpp
@@ -1,4 +1,5 @@
 // single entry point to mikado
 
+#include "helper.hpp"
 #include "linear_solver.hpp"
 #include "nonlinear_solver.hpp"
--- test/t.cpp
+++ test/t.cpp
@@ -2,8 +2,25 @@
 
 #include <mikado.hpp>
 
+using dict = std::map<std::string, boost::any>;
+
 // ===========================================================================
 TEST_CASE("dummy test", "[dummy]")
 {
+  const dict & map =
+  {
+    {"method", "Pseudo Block CG"},
+    {"parameters", dict{
+      {"Convergence Tolerance", 1.0e-10},
+      {"Output Frequency", 1},
+      {"Output Style", 1},
+      {"Verbosity", 33}
+    }},
+    {"preconditioner", "MueLu"}
+  };
+
+  const auto out = mikado::convert_to_belos_parameters(map);
+
+  mikado::show_map(out);
 }
 // ============================================================================
