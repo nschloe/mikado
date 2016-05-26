4d21b205e1eb45df6c50f850d53d2f94166304bb
Nico Schlömer <nico.schloemer@gmail.com>
2016-05-26 23:05:09 +0200

    fixes for older compiler

--- test/t.cpp
+++ test/t.cpp
@@ -9,14 +9,14 @@ TEST_CASE("dummy test", "[dummy]")
 {
   const dict & map =
   {
-    {"method", "Pseudo Block CG"},
+    {"method", std::string("Pseudo Block CG")},
     {"parameters", dict{
       {"Convergence Tolerance", 1.0e-10},
       {"Output Frequency", 1},
       {"Output Style", 1},
       {"Verbosity", 33}
     }},
-    {"preconditioner", "MueLu"}
+    {"preconditioner", std::string("MueLu")}
   };
 
   const auto out = mikado::convert_to_belos_parameters(map);
