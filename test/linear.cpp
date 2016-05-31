d12f3b252a57659ad556ff7c03b2405d409a4915
Nico Schlömer <nico.schloemer@gmail.com>
2016-06-01 01:28:55 +0200

    belos without preconditioner

--- test/linear.cpp
+++ test/linear.cpp
@@ -69,7 +69,36 @@ TEST_CASE("default solver", "[default]")
   }
 }
 // ===========================================================================
-TEST_CASE("Belos solver", "[belos]")
+TEST_CASE("Belos solver (no preconditioner)", "[belos no prec]")
+{
+  const auto comm = Teuchos::DefaultComm<int>::getComm();
+
+  const auto A = create_matrix(comm);
+
+  mikado::show_tpetra_crs_matrix(*A);
+
+  auto b = Tpetra::Vector<double,int,int>(A->getRangeMap());
+  b.putScalar(1.0);
+
+  auto x = Tpetra::Vector<double,int,int>(A->getDomainMap());
+  x.putScalar(1.0);
+
+  mikado::linear_solve(
+      *A, b, x, dict{
+        {"package", std::string("Belos")},
+        {"method", std::string("Pseudo Block GMRES")}
+      }
+      );
+
+  const auto x_data = x.getData();
+  const auto myGlobalElements = x.getMap()->getNodeElementList();
+  REQUIRE(myGlobalElements.size() == x_data.size());
+  for (size_t i = 0; i < myGlobalElements.size(); i++) {
+    REQUIRE(x_data[i] == Approx(myGlobalElements[i] + 1));
+  }
+}
+// ===========================================================================
+TEST_CASE("Belos solver with MueLu preconditioner", "[belos muelu]")
 {
   const auto comm = Teuchos::DefaultComm<int>::getComm();
 
@@ -98,6 +127,36 @@ TEST_CASE("Belos solver", "[belos]")
     REQUIRE(x_data[i] == Approx(myGlobalElements[i] + 1));
   }
 }
+// ===========================================================================
+// TEST_CASE("Belos solver with Ifpack2 preconditioner", "[belos ifpack2]")
+// {
+//   const auto comm = Teuchos::DefaultComm<int>::getComm();
+//
+//   const auto A = create_matrix(comm);
+//
+//   mikado::show_tpetra_crs_matrix(*A);
+//
+//   auto b = Tpetra::Vector<double,int,int>(A->getRangeMap());
+//   b.putScalar(1.0);
+//
+//   auto x = Tpetra::Vector<double,int,int>(A->getDomainMap());
+//   x.putScalar(1.0);
+//
+//   mikado::linear_solve(
+//       *A, b, x, dict{
+//         {"package", std::string("Belos")},
+//         {"method", std::string("Pseudo Block GMRES")},
+//         {"preconditioner", std::string("Ifpack2")}
+//       }
+//       );
+//
+//   const auto x_data = x.getData();
+//   const auto myGlobalElements = x.getMap()->getNodeElementList();
+//   REQUIRE(myGlobalElements.size() == x_data.size());
+//   for (size_t i = 0; i < myGlobalElements.size(); i++) {
+//     REQUIRE(x_data[i] == Approx(myGlobalElements[i] + 1));
+//   }
+// }
 // ===========================================================================
 TEST_CASE("MueLu solver", "[muelu]")
 {
